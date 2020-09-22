849325a19e7f3e3474d10ec961e548d1560ec4bf
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-09-22 13:34:30 +0600

    custom user model and stream for anon login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,31 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // final FirebaseAuth _auth = FirebaseAuth.instanceFor();
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user obj based on User
+  CustomClassName _userFromFirebaseUser(User user) {
+    return user != null ? CustomClassName(uid: user.uid) : null;
+  }
+
+  Stream<CustomClassName> get user {
+    return _auth
+        .authStateChanges()
+        //.map((User user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
 
   // sign in with anon
   Future signInAnon() async {
+    // FirebaseApp a = await Firebase.initializeApp();
+    // final FirebaseAuth _auth = FirebaseAuth.instanceFor(app: a);
+
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
