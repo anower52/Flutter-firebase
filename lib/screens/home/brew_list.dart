bc662891e3692da0cc96954326347a66b569835b
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-10-09 04:44:50 +0600

    listing brew data and show with list tile

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,11 +12,16 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    brews.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
-    return Container();
+    // brews.forEach((brew) {
+    //   print(brew.name);
+    //   print(brew.sugars);
+    //   print(brew.strength);
+    // });
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
