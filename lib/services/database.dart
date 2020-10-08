7cffaf9c0448b2a62eea21e311d02358fe347728
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-10-08 22:44:11 +0600

    user record in firebase user record

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  //collection refrence
+  final String uid;
+  DatabaseService({this.uid});
 
+  //collection refrence
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.doc(uid).set({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
 }
