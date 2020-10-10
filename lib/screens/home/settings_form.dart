e316f7b4942a460cb4a4349838dda2ef9d4d4e86
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-10-11 05:40:50 +0600

    using stream builder for data show in seeting panel

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -17,75 +21,92 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            'Update your Coffee Brew settings',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextFormField(
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) {
-              setState(() {
-                _currentName = val;
-              });
-            },
-            decoration: textInputDecoration.copyWith(hintText: ('Name')),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          //dropdown
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0',
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                child: Text('$sugar sugars'),
-                value: sugar,
-              );
-            }).toList(),
-            onChanged: (val) {
-              setState(() {
-                _currentSugars = val;
-              });
-            },
-          ),
+    final user = Provider.of<CustomClassName>(context);
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
 
-          SizedBox(
-            height: 20.0,
-          ),
-          //slider
-          Slider(
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            min: 100.0,
-            max: 900.0,
-            divisions: 8,
-            onChanged: (val) => setState(() => _currentStrength = val.round()),
-          ),
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    'Update your Coffee Brew settings',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    initialValue: userData.name,
+                    validator: (val) =>
+                        val.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) {
+                      setState(() {
+                        _currentName = val;
+                      });
+                    },
+                    decoration:
+                        textInputDecoration.copyWith(hintText: ('Name')),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  //dropdown
+                  DropdownButtonFormField(
+                    decoration: textInputDecoration,
+                    value: _currentSugars ?? userData.sugars,
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        child: Text('$sugar sugars'),
+                        value: sugar,
+                      );
+                    }).toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        _currentSugars = val;
+                      });
+                    },
+                  ),
 
-          //update button
-          RaisedButton(
-            color: Colors.pink[400],
-            child: Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-          )
-        ],
-      ),
-    );
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  //slider
+                  Slider(
+                    value: (_currentStrength ?? userData.strength).toDouble(),
+                    activeColor:
+                        Colors.brown[_currentStrength ?? userData.strength],
+                    inactiveColor:
+                        Colors.brown[_currentStrength ?? userData.strength],
+                    min: 100.0,
+                    max: 900.0,
+                    divisions: 8,
+                    onChanged: (val) =>
+                        setState(() => _currentStrength = val.round()),
+                  ),
+
+                  //update button
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      'Update',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      print(_currentName);
+                      print(_currentSugars);
+                      print(_currentStrength);
+                    },
+                  )
+                ],
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
